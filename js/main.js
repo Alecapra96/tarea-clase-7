3e0a0cb99572afaf1799cca4f9b2452ef3c67ed8
alecapra96 <alejandrocapraargentina@gmail.com>
2021-01-25 12:07:59 -0300

    Arreglando tarea

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 const $form = document.querySelector("#carta-a-santa");
 let nombre = $form.nombre.value;
+console.log(nombre);
 let ciudad= $form.ciudad.value;
 let comportamiento= $form.comportamiento.value;
 let descripcionRegalo= $form["descripcion-regalo"].value;
+let boton= $form["enviar-carta"];
+console.log(boton);
+
 
 function validarNombre(nombre){
+
     if(nombre.length === 0 ){
         return "este campo debe tener al menos 1 caracter";
     }
     if(nombre.length >= 50){
-        return "este campo debe ser menor a 50 caracteres ";
+        return "este campo debe ser menor a 50 caracteres";
     }
-    return "";
+    else {
+        return "";
+    }
 }
 
 function validarCiudad(ciudad){
--- js/pruebas.js
+++ js/pruebas.js
@@ -1,38 +1,37 @@
 function probarCartaPapaNoel() {
-    console.assert(
-        validarNombre('') === 'Este campo debe tener al menos 1 caracter',
+    console.assert(validarNombre('') === 'este campo debe tener al menos 1 caracter',
         'Validar nombre no validó que el nombre no sea vacío',
     );
   
     console.assert(
         validarNombre(
             '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-        'Este campo debe tener menos de 50 caracteres',
+        'este campo debe ser menor a 50 caracteres',
         'Validar nombre no validó que el nombre sea menor a 50 caracteres',
     );
     console.assert(
         validarCiudad('') ===
-        'Este campo debe tener al menos 1 caracter',
+        'debes seleccionar algo',
         'ValidarCiudad no validó que el nombre no sea vacío',
     );
     console.assert(
         validarComportamiento('') ===
-        'Este campo debe tener al menos un iten seleccionado',
+        'debes seleccionar algo',
         'ValidarComportamiento no validó que al menos un iten este seleccionado',
     );
     console.assert(
-        validarDescripcionRegalo('') === 'Este campo debe tener al menos 1 caracter',
+        validarDescripcionRegalo('') === 'este campo debe tener al menos 1 caracter',
         'Validar nombre no validó que el nombre no sea vacío',
     );
   
     console.assert(
         validarDescripcionRegalo(
             'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa') ===
-        'Este campo debe tener menos de 350 caracteres',
+        'este campo debe ser menor a 350 caracteres ',
         'Validar nombre no validó que el nombre sea menor a 350 caracteres',
     );
     
   }
-probarCartaPapaNoel();
+ probarCartaPapaNoel();
   
   
